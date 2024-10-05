f35831bc3df355709034bb8493ed1f8440ba3f3e
Roman Yanush <roman.yanush7@gmail.com>
2024-10-05 17:40:06 +0300

    fix: update tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,40 @@
-import { TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import {NO_ERRORS_SCHEMA, signal} from "@angular/core";
+import {GameRunnerService} from "./services/game-runner/game-runner.service";
+import {Subject} from "rxjs";
+import {GameEvents} from "./core/enums";
+import {ModalService} from "./services/modal/modal.service";
+
+const mockGameEvents = new Subject<GameEvents>();
+
+const mockGameRunnerService = {
+  defaultTimeLimit: 1000,
+  startGame: jasmine.createSpy('startGame'),
+  gameEvents: mockGameEvents.asObservable(),
+  playerScore: signal(0)
+}
+
+const mockModalService = {
+  open: jasmine.createSpy('open'),
+}
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
+      providers: [
+        { provide: GameRunnerService, useValue: mockGameRunnerService },
+        { provide: ModalService, useValue: mockModalService }
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
   });
 
   it('should create the app', () => {
@@ -13,4 +42,27 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
+
+  it('should start game', () => {
+    component.startGame();
+    expect(mockGameRunnerService.startGame).toHaveBeenCalledWith(jasmine.objectContaining({
+      timeLimit: 1000
+    }))
+  });
+
+  describe('game ended', () => {
+    it('should subscribe for game events on init and message player won', () => {
+      mockGameRunnerService.playerScore.set(10);
+      component.ngOnInit();
+      mockGameEvents.next(GameEvents.GameEnded);
+      expect(mockModalService.open).toHaveBeenCalledWith('Player won');
+    });
+
+    it('should subscribe for game events on init and message player won', () => {
+      mockGameRunnerService.playerScore.set(9);
+      component.ngOnInit();
+      mockGameEvents.next(GameEvents.GameEnded);
+      expect(mockModalService.open).toHaveBeenCalledWith('Computer won');
+    });
+  })
 });
